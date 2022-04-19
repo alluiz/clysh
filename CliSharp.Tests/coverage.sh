c168326f9c9ff4ef1a061051c05af76ff521f54e
Luiz Almeida <al.luiz@live.com>
2022-04-19 03:13:25 -0300

    chore: remove session id from coverage shell file

--- CliSharp.Tests/coverage.sh
+++ CliSharp.Tests/coverage.sh
@@ -2,7 +2,7 @@
 
 now=$(date +"%Y%m%d_%H%M%S")
 
-dotnet-coverage collect "dotnet test" --output-format "cobertura" --output "./temp/${now}_coverage" --settings ./test.runsettings -id "${uuid}"
+dotnet-coverage collect "dotnet test" --output-format "cobertura" --output "./temp/${now}_coverage" --settings ./test.runsettings
 
 #HTML
 reportgenerator -reports:"./temp/${now}_coverage.cobertura.xml" -targetdir:"./coverage/report/html" -reporttypes:"Html" -historydir:"./coverage/history"
