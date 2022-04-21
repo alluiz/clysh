5953178f9fb7fdd4a1919d145bd91d473c3b9a61
Luiz Almeida <al.luiz@live.com>
2022-04-21 04:26:17 -0300

    fix: example program

---
+++ CliSharp.Example/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace CliSharp.App
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CliSharpDataSetup setup = new("clidata.yml");
+
+            setup.SetCommandAction("mycli", (options, cliFront) =>
+            {
+                if (options.Has("test"))
+                    cliFront.PrintWithBreak($"mycli with test option");
+                else
+                    cliFront.PrintWithBreak($"mycli without test option");
+            });
+
+            ICliSharpView view = new CliSharpView(new CliSharpConsole(), setup.Data, true);
+
+            ICliSharpService cli = new CliSharpService(setup.RootCommand, view);
+
+            cli.Execute(args);
+        }
+    }
+}
