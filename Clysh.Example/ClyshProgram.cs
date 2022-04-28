05c1861ec19eafebd735a3ce2e145b715f27324b
Luiz Almeida <al.luiz@live.com>
2022-04-28 03:48:07 -0300

    refact: surround example with try-catch block

--- Clysh.Example/ClyshProgram.cs
+++ Clysh.Example/ClyshProgram.cs
@@ -1,19 +1,39 @@
-﻿namespace Clysh.Example
+﻿using System;
+
+namespace Clysh.Example
 {
     public static class ClyshProgram
     {
         public static void Main(string[] args)
         {
-            ClyshDataSetup setup = new("clidata.yml");
-
-            setup.MakeAction("mycli", (options, view) =>
+            try
             {
-                view.Print(options.Has("test") ? "mycli with test option" : "mycli without test option");
-            });
-            
-            IClyshService cli = new ClyshService(setup);
+                ClyshDataSetup setup = new("clidata.yml");
 
-            cli.Execute(args);
+                setup.MakeAction("mycli", (options, view) =>
+                {
+                    view.Print(options.Has("test") ? "mycli with test option" : "mycli without test option");
+                });
+            
+                IClyshService cli = new ClyshService(setup);
+
+                cli.Execute(args);
+            }
+            catch (ArgumentNullException e)
+            {
+                Console.Write(e);
+                Environment.ExitCode = 3;
+            }
+            catch (ArgumentException e)
+            {
+                Console.Write(e);
+                Environment.ExitCode = 2;
+            }
+            catch (Exception e)
+            {
+                Console.Write(e);
+                Environment.ExitCode = 1;
+            }
         }
     }
 }
