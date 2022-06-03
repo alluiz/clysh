d90c38d6c780512ddca0b27b5892800eaa231165
Luiz Almeida <al.luiz@live.com>
2022-06-03 03:59:37 -0300

    Update ClyshParameterData.cs

--- Clysh.Data/ClyshParameterData.cs
+++ Clysh.Data/ClyshParameterData.cs
@@ -7,30 +7,6 @@ namespace Clysh.Data
     public class ClyshParameterData
     {
         /// <summary>
-        /// Create a <b>ClyshParameterData</b> object
-        /// </summary>
-        public ClyshParameterData()
-        {
-        }
-
-        /// <summary>
-        /// Create a <b>ClyshParameterData</b> object
-        /// </summary>
-        /// <param name="id">The id of parameter</param>
-        /// <param name="pattern">The regular expression pattern</param>
-        /// <param name="required">Indicates if is a required parameter</param>
-        /// <param name="minLength">The minimum length</param>
-        /// <param name="maxLength">The maximum length</param>
-        public ClyshParameterData(string id, string? pattern, bool required, int minLength, int maxLength)
-        {
-            Id = id;
-            Pattern = pattern;
-            Required = required;
-            MinLength = minLength;
-            MaxLength = maxLength;
-        }
-        
-        /// <summary>
         /// The id of parameter
         /// </summary>
         public string Id { get; set; } = null!;
